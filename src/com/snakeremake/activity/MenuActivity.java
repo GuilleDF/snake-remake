ae99d48ff7fb50777ff3f146bce8446a0f0bc4a2
GuilleDF <gdefermin@gmail.com>
2014-07-20 17:41:48 +0200

    Fixed menu

--- src/com/snakeremake/activity/MenuActivity.java
+++ src/com/snakeremake/activity/MenuActivity.java
@@ -17,12 +17,10 @@ public class MenuActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-		//We start the splash activity, which will close itself when it's done
-		startActivity(new Intent(this, SplashActivity.class));
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		//		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				
 		
 		setContentView(R.layout.menu_layout);
 				
